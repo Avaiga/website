75e03266bcd964f7312ad18aa38be0444505ddb4
Andrew Golovanov <spacewebdeveloper@gmail.com>
2024-03-11 13:52:04 +0100

    feat: initial settings

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,19 @@
+import { Inter } from 'next/font/google';
+
 import Footer from '@/components/shared/footer';
 import Header from '@/components/shared/header/header';
 
 import '@/styles/globals.css';
 
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-inter',
+});
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" className={inter.variable}>
       <body>
         <Header />
         <main>{children}</main>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,32 +5,33 @@ module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     fontFamily: {
-      // FIXME: Add the real project font families, which are imported in the "fonts.css" file,
-      // and give them the human-readable names
-      sans: ['', ...defaultTheme.fontFamily.sans],
-      // FIXME: Remove the "mono" font family, if it is not used in the project
-      mono: ['', ...defaultTheme.fontFamily.mono],
+      sans: ['var(--font-inter)', ...defaultTheme.fontFamily.sans],
     },
     colors: ({ colors }) => ({
       inherit: colors.inherit,
       current: colors.current,
       transparent: colors.transparent,
-      // FIXME: Add colors which are used in the project. Make sure that they are prepared
-      // in the Figma and followed the naming "primary/secondary/gray-${number}"
-      // Example of correctly prepared colors in Figma — https://user-images.githubusercontent.com/20713191/143586876-5e834233-9639-4166-9811-b00e63820d98.png
-      black: '',
-      white: '',
-      primary: {
-        1: '',
-      },
-      secondary: {
-        1: '',
+      black: '#070709',
+      white: '#ffffff',
+      orange: {
+        1: '#FF462B',
+        70: '#ECA979',
       },
       gray: {
-        1: '',
+        5: '#0b0c0f',
+        10: '#16181d',
+        15: '#22242a',
+        20: '#2e3038',
+        30: '#454954',
+        40: '#5e626e',
+        50: '#787c87',
+        60: '#9195a1',
+        70: '#adb0b9',
+        80: '#c8cad0',
+        90: '#e4e5e8',
+        94: '#efeff1',
       },
     }),
-    // FIXME: Check if the breakpoints ("screens") are correct for the project
     screens: {
       xl: { max: '1439px' },
       lg: { max: '1279px' },
