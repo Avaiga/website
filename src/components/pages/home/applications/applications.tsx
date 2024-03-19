de43f9d89183ddd2e6906cdbee381fd0c1a0955c
iatopilskii <iatopilskii@gmail.com>
2024-03-19 18:26:01 +0100

    fix: remove unnecessary classes

--- src/components/pages/home/applications/applications.tsx
+++ src/components/pages/home/applications/applications.tsx
@@ -54,18 +54,18 @@ function Applications() {
           Taipy holds no opinions on what&apos;s and how&apos;s. Build simple pilots, large-scale
           complex apps, or anything in between. Explore demos for inspiration.
         </p>
-        <div className="mt-16 grid grid-cols-12 gap-8">
+        <div className="mt-16 grid grid-cols-3 gap-8">
           {CARDS.map(({ image, title, description }, index) => (
-            <div key={index} className="card-border col-span-4 h-[384px] rounded-lg">
+            <div key={index} className="card-border h-[384px] rounded-lg">
               <div className="relative flex h-full w-full flex-col justify-end rounded-lg bg-grey-10 p-8">
                 <Image
-                  className="pointer-events-none -z-0 object-contain object-top"
+                  className="pointer-events-none object-contain object-top"
                   src={image}
                   alt={title}
                   fill
                 />
-                <h3 className="relative text-20 font-semibold leading-snug">{title}</h3>
-                <p className="relative mt-2 text-18 text-grey-70">{description}</p>
+                <h3 className="text-20 font-semibold leading-snug">{title}</h3>
+                <p className="mt-2 text-18 text-grey-70">{description}</p>
               </div>
             </div>
           ))}
