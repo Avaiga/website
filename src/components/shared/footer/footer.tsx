832a75aab0cf8651c7dd0374ed2db0323211fbb0
iatopilskii <iatopilskii@gmail.com>
2024-03-25 19:00:12 +0100

    feat: footer block adaptive

--- src/components/shared/footer/footer.tsx
+++ src/components/shared/footer/footer.tsx
@@ -9,14 +9,14 @@ import logo from '@/svgs/logo.svg';
 
 function Footer() {
   return (
-    <footer className="relative bg-[linear-gradient(89.86deg,rgba(41,41,45,0.6)_-34.54%,rgba(19,19,21,0)_104.53%)] pt-16 px-safe pb-safe-or-12 before:absolute before:-top-px before:left-0 before:h-px before:w-full before:bg-[linear-gradient(90deg,#383839_4%,#5D2822_16%,#1E1E21_30%,#151517_54.82%)]">
+    <footer className="relative bg-[linear-gradient(89.86deg,rgba(41,41,45,0.6)_-34.54%,rgba(19,19,21,0)_104.53%)] pt-16 px-safe pb-safe-or-12 before:absolute before:-top-px before:left-0 before:h-px before:w-full before:bg-[linear-gradient(90deg,#383839_4%,#5D2822_16%,#1E1E21_30%,#151517_54.82%)] lg:pt-12 lg:pb-safe-or-10">
       <div className="container">
         <div className="flex items-start justify-between">
           <Link href="/">
             <span className="sr-only">Taipy</span>
             <Image className="h-7" src={logo} width={109} height={28} alt="Taipy logo" />
           </Link>
-          <nav className="flex gap-x-[106px]">
+          <nav className="flex gap-x-[106px] lg:mr-[58px] lg:w-full lg:max-w-[582px] lg:justify-between lg:gap-x-0">
             {MENU.footer.main.map(({ title, items }, index) => (
               <div key={index}>
                 <span className="text-16 leading-normal text-[#ADB0B9]">{title}</span>
@@ -34,7 +34,7 @@ function Footer() {
           </nav>
         </div>
 
-        <div className="mt-[140px] flex items-end justify-between">
+        <div className="mt-[140px] flex items-end justify-between lg:mt-[89px]">
           <div className="flex gap-x-6">
             {MENU.footer.social.map(({ href, icon: Icon }, index) => (
               <Link href={href} key={index}>
@@ -43,7 +43,12 @@ function Footer() {
             ))}
           </div>
 
-          <Link className="-ml-[54px] font-medium" size="sm" theme="grey" href={ROUTE.PRIVACY}>
+          <Link
+            className="ml-[-54px] font-medium lg:ml-[-280px]"
+            size="sm"
+            theme="grey"
+            href={ROUTE.PRIVACY}
+          >
             Privacy
           </Link>
 
