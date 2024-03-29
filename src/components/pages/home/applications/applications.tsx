693a16b88b12b78324b32a9f6cd4f5675e36908f
iatopilskii <iatopilskii@gmail.com>
2024-03-29 12:27:57 +0100

    feat: applications block adaptive

--- src/components/pages/home/applications/applications.tsx
+++ src/components/pages/home/applications/applications.tsx
@@ -44,20 +44,20 @@ const CARDS = [
 
 function Applications() {
   return (
-    <section className="applications mt-[200px] px-safe lg:mt-[132px] md:mt-24">
+    <section className="applications mt-[200px] px-safe lg:mt-[132px] md:mt-24 sm:mt-[76px]">
       <div className="container max-w-7xl text-center">
         <span className="text-center leading-snug text-primary-red md:text-14">Applications</span>
-        <h2 className="mt-4 text-center text-56 font-semibold leading-dense tracking-tight lg:mt-3.5 lg:text-48 md:text-40">
-          Build anything, use Python only
+        <h2 className="mt-4 text-center text-56 font-semibold leading-dense tracking-tight lg:mt-3.5 lg:text-48 md:text-40 sm:mt-3 sm:text-28">
+          Build anything, use&nbsp;Python&nbsp;only
         </h2>
-        <p className="mx-auto mt-6 max-w-3xl text-center text-18 font-light text-grey-70 lg:mt-5 lg:max-w-[646px] lg:text-16 md:mt-4 md:max-w-[503px] md:text-14">
+        <p className="mx-auto mt-6 max-w-3xl text-center text-18 font-light text-grey-70 lg:mt-5 lg:max-w-[646px] lg:text-16 md:mt-4 md:max-w-[503px] md:text-14 sm:mt-3">
           Taipy holds no opinions on what&apos;s and how&apos;s. Build simple pilots, large-scale
           complex apps, or anything in between. Explore demos for inspiration.
         </p>
-        <div className="mt-14 grid grid-cols-3 gap-8 lg:mt-12 lg:gap-6 md:mt-14 md:grid-cols-2 md:px-8">
+        <div className="mt-14 grid grid-cols-3 gap-8 lg:mt-12 lg:gap-6 md:mt-14 md:grid-cols-2 md:px-8 sm:mt-6 sm:flex sm:flex-wrap sm:justify-center sm:gap-5 sm:px-0">
           {CARDS.map(({ image, title, description }, index) => (
             <div
-              className="card-border flex h-96 rounded-lg text-left lg:aspect-[1.013] lg:h-auto"
+              className="card-border flex h-96 rounded-lg text-left lg:aspect-[1.013] lg:h-auto sm:aspect-auto sm:h-[304px] sm:w-full sm:max-w-[320px]"
               key={index}
             >
               <div className="relative flex w-full flex-col justify-end rounded-lg bg-grey-10 p-8 lg:p-6">
@@ -72,7 +72,7 @@ function Applications() {
             </div>
           ))}
         </div>
-        <div className="mt-[50px] flex justify-center lg:mt-10 md:mt-9">
+        <div className="mt-[50px] flex justify-center lg:mt-10 md:mt-9 sm:mt-6">
           <Link className="leading-snug" href="/" size="lg" theme="white" arrowTheme="red">
             See all demos
           </Link>
