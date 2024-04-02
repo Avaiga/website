306e70ed4bbd133e29257b02d7420b0e4fb32bf9
Denis <olga.volotovich@mail.ru>
2024-04-02 15:41:15 +0200

    refactor: styles of navbar

--- src/components/pages/blog/sidebar/nav-list/nav-list.tsx
+++ src/components/pages/blog/sidebar/nav-list/nav-list.tsx
@@ -13,12 +13,12 @@ interface NavListProps {
 export default function NavList({ data }: NavListProps) {
   return (
     <nav>
-      <ul className="flex flex-col gap-y-2.5 bg-transparent text-grey-60 lg:flex-row lg:gap-x-[23px]">
+      <ul className="flex flex-col gap-y-2.5 bg-transparent text-grey-60 lg:flex-row lg:gap-x-6">
         {data.map(({ label, href }, index) => (
           <li key={index}>
             <Link
               className={clsx(
-                'block border-l-2 pb-[5px] pl-3 pt-1 text-15 leading-[1.47] -tracking-[0.01em] transition-all hover:border-l-primary-red hover:text-white lg:border-b-2 lg:border-l-0 lg:pb-px lg:pl-0 lg:text-14',
+                'block border-l-2 pb-[5px] pl-3 pt-1 text-15 leading-normal tracking-snug transition-all hover:border-l-primary-red hover:text-white lg:border-b-2 lg:border-l-0 lg:pb-px lg:pl-0 lg:text-14 ',
                 {
                   'border-l-primary-red text-white lg:border-b-primary-red': index === 0,
                   'border-l-transparent lg:border-b-transparent': index !== 0,
