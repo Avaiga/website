7729088ee38a6e73c6d8ecbbdaa380bcbc27559c
Denis <olga.volotovich@mail.ru>
2024-04-02 16:50:36 +0200

    fix: missprint

--- src/components/pages/blog/sidebar/nav-list/nav-list.tsx
+++ src/components/pages/blog/sidebar/nav-list/nav-list.tsx
@@ -18,7 +18,7 @@ export default function NavList({ data }: NavListProps) {
           <li key={index}>
             <Link
               className={clsx(
-                'block border-l-2 pb-[5px] pl-3 pt-1 text-15 leading-normal tracking-snug  transition-all hover:border-l-primary-red hover:text-white lg:border-b-2 lg:border-l-0 lg:pb-px lg:pl-0 lg:text-14 ',
+                'block border-l-2 pb-[5px] pl-3 pt-1 text-15 leading-none tracking-snug transition-all hover:border-l-primary-red hover:text-white lg:border-b-2 lg:border-l-0 lg:pb-px lg:pl-0 lg:text-14 ',
                 {
                   'border-l-primary-red text-white lg:border-b-primary-red': index === 0,
                   'border-l-transparent lg:border-b-transparent': index !== 0,
