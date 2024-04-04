2d0af9413be888308b331d3e73dd2b8e913da2bf
Denis <olga.volotovich@mail.ru>
2024-04-04 14:02:59 +0200

    refactor: add sticky to tablet and mobile nav bar

--- src/components/pages/blog/sidebar/nav-list/nav-list.tsx
+++ src/components/pages/blog/sidebar/nav-list/nav-list.tsx
@@ -12,7 +12,7 @@ interface NavListProps {
 }
 export default function NavList({ data }: NavListProps) {
   return (
-    <nav className="no-scrollbars sticky top-4 px-5 sm:-mx-5 sm:overflow-auto">
+    <nav className="no-scrollbars sm:-mx-5 sm:overflow-auto sm:px-5">
       <ul className="flex flex-col gap-y-3 bg-transparent text-grey-60 lg:flex-row lg:gap-x-6 md:after:shrink-0 md:after:grow-0 md:after:basis-px md:after:content-['']">
         {data.map(({ label, href }, index) => (
           <li key={index}>
