dee3e8d84e505797871315722e968d2fa81a1a12
ZhRodion <jerdew.r@icloud.com>
2024-04-04 17:17:37 +0400

    fix: remove spaces

--- src/components/pages/about-us/hero/advantages-list/advantantages-list.tsx
+++ src/components/pages/about-us/hero/advantages-list/advantantages-list.tsx
@@ -1,22 +1,18 @@
 type AdvantagesItemProps = {
-  id: number;
   digit: string;
   description: string;
 };
 
 const advantagesData: AdvantagesItemProps[] = [
   {
-    id: 1,
     digit: '10x',
     description: 'reduction in lines of code',
   },
   {
-    id: 2,
     digit: '4x',
     description: 'reduction in project duration',
   },
   {
-    id: 3,
     digit: '5x',
     description: 'reduction in project costs',
   },
@@ -24,9 +20,9 @@ const advantagesData: AdvantagesItemProps[] = [
 
 export default function AdvantagesList() {
   return (
-    <ul className="mt-11 flex w-full justify-between gap-6 ">
-      {advantagesData.map((item) => (
-        <li className=" w-full text-center" key={item.id}>
+    <ul className="mt-11 flex w-full justify-between gap-6">
+      {advantagesData.map((item, index) => (
+        <li className=" w-full text-center" key={index}>
           <span className="bg-gradient-digit bg-clip-text text-[120px] font-light leading-none tracking-snug text-transparent">
             {item.digit}
           </span>
--- src/components/pages/about-us/hero/hero.tsx
+++ src/components/pages/about-us/hero/hero.tsx
@@ -5,7 +5,7 @@ export default function Hero() {
     <section className="hero pt-[144px]">
       <div className="container-narrow ">
         <div className="text-center">
-          <span className=" text-16 font-normal leading-snug text-primary-red">About us</span>
+          <span className="text-16 font-normal leading-snug text-primary-red">About us</span>
           <h1 className="mx-auto mt-4 w-full max-w-[881px] text-56 font-medium leading-dense tracking-tight">
             We provide an open-source Python full-stack application builder
           </h1>
