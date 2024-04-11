4797816893cd48cbc776856d475566d412fef9af
Zherdev <jerdew.r@icloud.com>
2024-04-11 11:14:21 +0400

    feat: add 2/3 desktop adaptive

--- src/components/pages/about-us/our-history/history-list/history-list.tsx
+++ src/components/pages/about-us/our-history/history-list/history-list.tsx
@@ -1,20 +1,36 @@
+import { SVGProps } from 'react';
+
+import CollaborationIcon from '@/svgs/pages/about-us/our-history/collaboration.inline.svg';
+import UsersIcon from '@/svgs/pages/about-us/our-history/users.inline.svg';
 import PythonIcon from '@/svgs/pages/home/benefits/python.inline.svg';
 
+type ExtendedSVGProps<T extends SVGElement> = SVGProps<T> & {
+  alt?: string;
+};
+
 type historyListProps = {
+  logo: React.FunctionComponent<ExtendedSVGProps<SVGSVGElement>>;
+  alt: string;
   heading: string;
   text: string;
 };
 
 const historyListData: historyListProps[] = [
   {
+    logo: CollaborationIcon,
+    alt: 'Collaborative data processing',
     heading: 'Collaborative data processing',
     text: 'Including Data Scientists, Data Engineers, GUI developers, MLOps, as reported by Gartner saying that more than 85% of pilots developed in Python remain pilots.',
   },
   {
+    logo: PythonIcon,
+    alt: 'Python tools for full-stack development',
     heading: 'Python tools for full-stack development',
     text: 'Taipy provides both frontend and backend components, enabling full-stack development in Python. The frontend allows for easy creation of GUI using Markdown language, while the backend schedules tasks and optimizes performance for efficient full-stack development',
   },
   {
+    logo: UsersIcon,
+    alt: 'User-centric approach',
     heading: 'User-centric approach',
     text: 'A lack of Python tools for both front-end and back-end development. With a fast learning curve and robust application-building capabilities.',
   },
@@ -22,14 +38,14 @@ const historyListData: historyListProps[] = [
 
 export default function HistoryList() {
   return (
-    <ul className="flex flex-col gap-6 pt-10">
-      {historyListData.map((item, index) => (
+    <ul className="flex flex-col gap-6 pt-10 md:gap-6 md:pt-[45px]">
+      {historyListData.map(({ logo: Logo, heading, text, alt }, index) => (
         <li key={index}>
           <div className="flex items-center gap-3">
-            <PythonIcon className="h-[24px] w-[24px]" />
-            <h3 className="text-24 font-medium leading-normal">{item.heading}</h3>
+            <Logo className="h-[24px] w-[24px] fill-white" alt={alt} />
+            <h3 className="text-24 font-medium leading-normal md:leading-tight">{heading}</h3>
           </div>
-          <p className="pt-2 font-light leading-normal text-grey-70">{item.text}</p>
+          <p className="pt-2 font-light leading-normal text-grey-70">{text}</p>
         </li>
       ))}
     </ul>
---
+++ src/components/pages/enterprise/hero/text-cards/text-cards.tsx
@@ -0,0 +1,31 @@
+import Image from 'next/image';
+
+import Scalability from '@/svgs/pages/enterprise/hero/scalability-icon.svg';
+import User from '@/svgs/pages/enterprise/hero/users-icon.svg';
+
+const textCardsData = [
+  {
+    icon: Scalability,
+    heading: 'Performance & Scalability',
+    text: 'Taipy Enterprise excels in backend operations, effortlessly managing large-scale tasks across CPU or GPU clusters and providing advanced Telemetry controls for monitoring.',
+  },
+  {
+    icon: User,
+    heading: 'User management',
+    text: 'Easily tailor access levels for different user groups, granting full application access to some and restricting others to specific features like pages, buttons, or charts.',
+  },
+];
+
+export default function TextCards() {
+  return (
+    <ul className="mx-auto mt-[69px] flex w-full max-w-[896px] justify-between text-start">
+      {textCardsData.map(({ icon, heading, text }, index) => (
+        <li className="w-full max-w-[384px]" key={index}>
+          <Image className="" src={icon} width={24} height={24} alt={heading} />
+          <h2 className="mt-4 block text-20 font-medium leading-dense tracking-tight">{heading}</h2>
+          <p className="mt-4 font-light leading-normal text-grey-70">{text}</p>
+        </li>
+      ))}
+    </ul>
+  );
+}
