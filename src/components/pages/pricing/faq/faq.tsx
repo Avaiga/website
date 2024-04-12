09dc1992c03ec6bc8adbe8217c7e636659b65391
Kirill Bolotskii <kirill.bolotsky@gmail.com>
2024-04-12 15:11:45 +0200

    fix: faq content

--- src/components/pages/pricing/faq/faq.tsx
+++ src/components/pages/pricing/faq/faq.tsx
@@ -2,10 +2,12 @@ import Heading from '@/components/pages/pricing/heading/heading';
 
 import Item from './item';
 
+// @TODO: revisit this component, seems a bit too complex
+
 const items = [
   {
     question: 'Is pricing based on users or features?',
-    answer: `Pricing is based on users and not features.There are two types of users: `,
+    answer: `Pricing is based on users and not features. There are two types of users: `,
     hasListOptions: true,
     options: [
       `Development Users - Creators, builders, developers that are responsible for creating the application.`,
@@ -15,30 +17,24 @@ const items = [
   },
   {
     question: 'Do you require a special license for Testing, Stagings and Backups?',
-    answer: `Pricing is based on users and not features.There are two types of users: `,
-    hasListOptions: true,
-    options: [
-      `Development Users - Creators, builders, developers that are responsible for creating the application.`,
-      `Deployment Users – Business Users, users that are responsible for interacting with the application.`,
-    ],
+    answer: `No, a special license is not required. You have complete flexibility how you chose to use your licenses.`,
+    hasListOptions: false,
   },
   {
     question: 'Does integration cost extra?',
-    answer: `Pricing is based on users and not features.There are two types of users: `,
-    hasListOptions: true,
-    options: [
-      `Development Users - Creators, builders, developers that are responsible for creating the application.`,
-      `Deployment Users – Business Users, users that are responsible for interacting with the application.`,
-    ],
+    answer: `No, we do not charge an extra fee for integration. In-fact we provide examples on how to integrate with disparate data sources and platforms, for example Databricks, Dataiku, Snowflake`,
+    hasListOptions: false,
   },
   {
     question: 'Is the price dependent on the Cloud provider?',
-    answer: 'Pricing is based on users and not features.There are two types of users: ',
+    answer:
+      'No, Taipy is cloud agnostic. Taipy applications can be deployed on any cloud platform regardless of where the data resides and how it is consumed.',
     hasListOptions: false,
   },
   {
     question: 'Is support included?',
-    answer: 'Pricing is based on users and not features.There are two types of users: ',
+    answer:
+      'Yes, support is included as part of the license subscription fee. Specialized/custom support is available on request.',
     hasListOptions: false,
   },
 ];
