4997f9be26b09190a0dc4e1a35bf4624da146b82
Olga <olga.volotovich@mail.ru>
2024-04-30 19:49:04 +0200

    refactor: links removing

--- src/components/pages/contact-us/locations/locations.tsx
+++ src/components/pages/contact-us/locations/locations.tsx
@@ -2,23 +2,18 @@ import Image from 'next/image';
 
 import locationIcon from '@/images/pages/contact-us/location.svg';
 
-import Link from '@/components/shared/link';
-
 const locationsData = [
   {
     heading: 'Singapore 🇸🇬',
     address: '80 Robinson Road, 068898 Singapore',
-    link: '/',
   },
   {
     heading: 'France 🇫🇷',
     address: '6 Boulevard Dubreuil, 91400 Orsay, France',
-    link: '/',
   },
   {
     heading: 'USA 🇺🇸',
     address: '104 Bond Court, Los Gatos, California 95030',
-    link: '/',
   },
 ];
 
@@ -30,7 +25,7 @@ function Locations() {
           Locate where we&apos;re based
         </h2>
         <div className="mx-auto mt-16 grid max-w-[832px] grid-cols-3 gap-x-8 lg:mt-14 lg:max-w-[704px] lg:gap-x-16 md:mt-11 sm:mt-10 sm:grid-cols-2 sm:gap-x-4 sm:gap-y-11 sm:pr-4">
-          {locationsData.map(({ heading, address, link }, index) => {
+          {locationsData.map(({ heading, address }, index) => {
             return (
               <div key={index}>
                 <Image
@@ -46,14 +41,6 @@ function Locations() {
                 <p className="mb-2 mt-1.5 text-18 font-light leading-normal text-grey-70 lg:text-16 md:mb-1.5 md:mt-0.5 sm:text-14">
                   {address}
                 </p>
-                <Link
-                  className="text-16 leading-snug lg:text-15 sm:text-14"
-                  href={link}
-                  theme="white"
-                  arrowTheme="red"
-                >
-                  Learn more
-                </Link>
               </div>
             );
           })}
