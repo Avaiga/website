6ba7fc43fe62d9dd9c3672f4528f337dcb5ae8af
Olga <olga.volotovich@mail.ru>
2024-05-06 11:43:09 +0200

    feat: add svgs for countries

--- src/components/pages/contact-us/locations/locations.tsx
+++ src/components/pages/contact-us/locations/locations.tsx
@@ -1,19 +1,26 @@
 import Image from 'next/image';
 
+import FR from '@/images/country-svgs/FR.svg';
+import SG from '@/images/country-svgs/SG.svg';
+import US from '@/images/country-svgs/US.svg';
 import locationIcon from '@/images/pages/contact-us/location.svg';
 
+// TODO: implement countryIcon name generator by country name
 const locationsData = [
   {
-    heading: 'Singapore 🇸🇬',
+    heading: 'Singapore',
     address: '80 Robinson Road, 068898 Singapore',
+    countryIcon: SG,
   },
   {
-    heading: 'France 🇫🇷',
+    heading: 'France',
     address: '6 Boulevard Dubreuil, 91400 Orsay, France',
+    countryIcon: FR,
   },
   {
-    heading: 'USA 🇺🇸',
+    heading: 'USA',
     address: '104 Bond Court, Los Gatos, California 95030',
+    countryIcon: US,
   },
 ];
 
@@ -25,7 +32,7 @@ function Locations() {
           Locate where we&apos;re based
         </h2>
         <div className="mx-auto mt-16 grid max-w-[832px] grid-cols-3 gap-x-8 lg:mt-14 lg:max-w-[704px] lg:gap-x-16 md:mt-11 sm:mt-10 sm:grid-cols-2 sm:gap-x-4 sm:gap-y-11 sm:pr-4">
-          {locationsData.map(({ heading, address }, index) => {
+          {locationsData.map(({ heading, address, countryIcon }, index) => {
             return (
               <div key={index}>
                 <Image
@@ -35,9 +42,12 @@ function Locations() {
                   height={40}
                   alt=""
                 />
-                <h3 className="mt-[18px] text-24 font-medium leading-normal lg:mt-4 lg:text-20 md:mt-3.5 sm:mt-3 sm:text-18">
-                  {heading}
-                </h3>
+                <div className="mt-[18px] flex items-center gap-1.5 lg:mt-4 md:mt-3.5 sm:mt-3">
+                  <h3 className="text-24 font-medium leading-normal lg:text-20 sm:text-18">
+                    {heading}
+                  </h3>
+                  <img className="w-3.5" src={countryIcon} alt={heading} />
+                </div>
                 <p className="mb-2 mt-1.5 text-18 font-light leading-normal text-grey-70 lg:text-16 md:mb-1.5 md:mt-0.5 sm:text-14">
                   {address}
                 </p>
