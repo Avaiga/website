1551d387a2919bbf2b74571d62b598d7f37ce626
Olga <olga.volotovich@mail.ru>
2024-05-10 01:09:01 +0200

    refactor: add team member to about us, change flags to svgs

--- src/components/pages/about-us/our-team/team-list/team-list.tsx
+++ src/components/pages/about-us/our-team/team-list/team-list.tsx
@@ -1,5 +1,10 @@
 import Image from 'next/image';
 
+import FR from '@/images/country-svgs/FR.svg';
+import SG from '@/images/country-svgs/SG.svg';
+import TH from '@/images/country-svgs/TH.svg';
+import US from '@/images/country-svgs/US.svg';
+import VN from '@/images/country-svgs/VN.svg';
 import albertAntoine from '@/images/pages/about-us/team/albert-antoine.jpg';
 import alexandreSajus from '@/images/pages/about-us/team/alexandre-sajus.jpg';
 import desmondDelandro from '@/images/pages/about-us/team/desmond-delandro.jpg';
@@ -11,6 +16,7 @@ import longNguyenDinh from '@/images/pages/about-us/team/long-nguyen-dinh.jpg';
 import marineGosselin from '@/images/pages/about-us/team/marine-gosselin.jpg';
 import myriamDelarue from '@/images/pages/about-us/team/myriam-delarue.jpg';
 import namNguyen from '@/images/pages/about-us/team/nam-nguyen.jpg';
+import nevoDavid from '@/images/pages/about-us/team/nevo-david.jpg';
 import prNgoBaoChau from '@/images/pages/about-us/team/pr-ngo-bao-chau.jpg';
 import rymMichaut from '@/images/pages/about-us/team/rym-michaut.jpg';
 import toanQuach from '@/images/pages/about-us/team/toan-quach.jpg';
@@ -21,106 +27,112 @@ import vinhNguyen from '@/images/pages/about-us/team/vinh-nguyen.jpg';
 const teamListData = [
   {
     photo: vincentGosselin,
-    name: 'Vincent Gosselin 🇫🇷',
+    name: 'Vincent Gosselin',
     position: 'CEO & Co-founder‍',
-    alt: 'Vincent Gosselin',
+    countryIcon: FR,
   },
   {
     photo: albertAntoine,
-    name: 'Albert Antoine 🇸🇬',
+    name: 'Albert Antoine',
     position: 'Director & Co-founder‍',
-    alt: 'Albert Antoine',
+    countryIcon: SG,
   },
   {
     photo: fabienLelaquais,
-    name: 'Fabien Lelaquais 🇫🇷',
+    name: 'Fabien Lelaquais',
     position: 'CTO‍',
-    alt: 'Fabien Lelaquais',
+    countryIcon: FR,
   },
   {
     photo: jeanRobinMedori,
-    name: 'Jean-Robin Medori 🇺🇸',
+    name: 'Jean-Robin Medori',
     position: 'CPO',
-    alt: 'Jean-Robin Medori',
+    countryIcon: US,
   },
   {
     photo: myriamDelarue,
-    name: 'Myriam Delarue 🇫🇷',
+    name: 'Myriam Delarue',
     position: 'Administration and Finance Manager',
-    alt: 'Myriam Delarue',
+    countryIcon: FR,
   },
   {
     photo: desmondDelandro,
-    name: 'Desmond Delandro 🇺🇸',
+    name: 'Desmond Delandro',
     position: 'Senior Advisor to the board',
-    alt: 'Desmond Delandro',
+    countryIcon: US,
   },
 
   {
     photo: prNgoBaoChau,
-    name: 'Pr. Ngo Bao Chau 🇫🇷',
+    name: 'Pr. Ngo Bao Chau',
     position: 'Senior Advisor to the board',
-    alt: 'Pr. Ngo Bao Chau',
+    countryIcon: FR,
   },
   {
     photo: vinhNguyen,
-    name: 'Vinh Nguyen 🇻🇳',
+    name: 'Vinh Nguyen',
     position: 'CFO',
-    alt: 'Vinh Nguyen',
+    countryIcon: VN,
   },
   {
     photo: rymMichaut,
-    name: 'Rym Michaut 🇫🇷',
+    name: 'Rym Michaut',
     position: 'Global Community Manager',
-    alt: 'Rym Michaut',
+    countryIcon: FR,
   },
   {
     photo: florianJacta,
-    name: 'Florian Jacta 🇫🇷',
+    name: 'Florian Jacta',
     position: 'Community Success Engineer',
-    alt: 'Florian Jacta',
+    countryIcon: FR,
   },
   {
     photo: marineGosselin,
-    name: 'Marine Gosselin 🇫🇷',
+    name: 'Marine Gosselin',
     position: 'Developer Advocate',
-    alt: 'Marine Gosselin',
+    countryIcon: FR,
   },
   {
     photo: alexandreSajus,
-    name: 'Alexandre Sajus 🇫🇷',
+    name: 'Alexandre Sajus',
     position: 'Customer Success Engineer',
-    alt: 'Alexandre Sajus',
+    countryIcon: FR,
   },
   {
     photo: truongGiangDo,
-    name: 'Truong Giang Do 🇻🇳',
+    name: 'Truong Giang Do',
     position: 'Python Developer',
-    alt: 'Truong Giang Do',
+    countryIcon: VN,
   },
   {
     photo: fredericLefevereLaoide, // frederic-lefevere-laoide
-    name: 'Frédéric Lefévère-Laoide 🇫🇷',
+    name: 'Frédéric Lefévère-Laoide',
     position: 'Senior Software Engineer',
-    alt: 'Frédéric Lefévère-Laoide',
+    countryIcon: FR,
   },
   {
     photo: longNguyenDinh, // long-nguyen-dinh
-    name: 'Long Nguyen Dinh 🇻🇳',
+    name: 'Long Nguyen Dinh',
     position: 'Software Engineer',
-    alt: 'Long Nguyen Dinh',
+    countryIcon: VN,
   },
   {
     photo: toanQuach, // toan-quach
-    name: 'Toan Quach 🇻🇳',
+    name: 'Toan Quach',
     position: 'Backend Engineer',
-    alt: 'Toan Quach',
+    countryIcon: VN,
   },
   {
     photo: namNguyen, // nam-nguyen
-    name: 'Nam Nguyen 🇻🇳',
+    name: 'Nam Nguyen',
     position: 'Software Engineer',
-    alt: 'Nam Nguyen',
+    countryIcon: VN,
+  },
+  {
+    photo: nevoDavid,
+    name: 'Nevo David',
+    position: 'Fractional CMO',
+    countryIcon: TH,
   },
 ];
 
@@ -128,7 +140,7 @@ export default function TeamList() {
   return (
     <div className="flex justify-center">
       <ul className="mx-auto mt-[74px] grid grid-cols-4 gap-x-14 gap-y-16 lg:mt-16 lg:gap-14 md:mt-[55px] md:grid-cols-3 sm:mt-9 sm:gap-8 xs:grid-cols-2">
-        {teamListData.map(({ photo, name, position, alt }, index) => (
+        {teamListData.map(({ photo, name, position, countryIcon }, index) => (
           <li
             className="flex w-full max-w-[174px] flex-col items-center gap-[18px] md:max-w-[152px] sm:max-w-[142px] sm:gap-3"
             key={index}
@@ -138,14 +150,15 @@ export default function TeamList() {
                 className="rounded-[8px] grayscale-[100%] filter"
                 src={photo}
                 fill={true}
-                alt={alt}
+                alt={`${name}-image.img`}
               />
             </div>
             <div className="">
-              <div className="flex items-center justify-center gap-1">
+              <div className="flex items-center justify-center gap-1.5">
                 <span className="whitespace-nowrap text-18 font-medium leading-none tracking-snug md:whitespace-normal md:text-center sm:text-16">
                   {name}
                 </span>
+                <img className="w-3.5" src={countryIcon} alt="" />
               </div>
               <p className="mt-2 text-center text-16 font-light leading-none tracking-snug text-grey-70 sm:text-13">
                 {position}
