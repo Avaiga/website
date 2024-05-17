1ccf36354e11e438f8677a5a9512fd36ab6d579c
Olga <olga.volotovich@mail.ru>
2024-05-17 17:42:20 +0200

    feat: move scripts under axeptio control

--- src/app/(website)/layout.tsx
+++ src/app/(website)/layout.tsx
@@ -27,14 +27,8 @@ async function RootLayout({ children }: { children: React.ReactNode }) {
     >
       <head>
         {process.env.NODE_ENV === 'production' && (
-          <>
-            <Script src="https://plausible.io/js/script.tagged-events.js" data-domain="taipy.io" />
-            <Script
-              src="https://tag.clearbitscripts.com/v1/pk_1733d1e9903ce4818fec6c5fdd8118cf/tags.js"
-              referrerPolicy="strict-origin-when-cross-origin"
-            />
-            <Script strategy="beforeInteractive" id="axeptio_overlay">
-              {`
+          <Script strategy="beforeInteractive" id="axeptio_overlay">
+            {`
                 window.axeptioSettings = {
                   clientId: "6630e24fbbf25171c10a2e12",
                   cookiesVersion: "taipy-en-EU",
@@ -44,20 +38,48 @@ async function RootLayout({ children }: { children: React.ReactNode }) {
                   e.async = true; e.src = "//static.axept.io/sdk.js";
                   t.parentNode.insertBefore(e, t);
                 })(document, "script");
+
+                function loadHotjar() {
+                  (function(h,o,t,j,a,r){
+                    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                    h._hjSettings={hjid:4945188,hjsv:6};
+                    a=o.getElementsByTagName('head')[0];
+                    r=o.createElement('script');r.async=1;
+                    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                    a.appendChild(r);
+                    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+                }
+
+                function loadPlausible() {
+                    const script = document.createElement('script'); 
+                    script.src = 'https://plausible.io/js/script.tagged-events.js'; 
+                    script.setAttribute('data-domain', 'taipy.io'); 
+                    document.head.appendChild(script); 
+                }
+
+                function loadClearbit() {
+                  const script = document.createElement('script'); 
+                  script.src = 'https://tag.clearbitscripts.com/v1/pk_1733d1e9903ce4818fec6c5fdd8118cf/tags.js'; 
+                  script.setAttribute('referrerpolicy', 'strict-origin-when-cross-origin'); 
+                  document.head.appendChild(script); 
+                }
+
+                void 0 === window._axcb && (window._axcb = []);
+                window._axcb.push(function (axeptio) {
+                  axeptio.on("cookies:complete", function (choices) {
+                    if (choices.hotjar) {
+                      loadHotjar();
+                    }
+                    if (choices.plausible) {
+                      loadPlausible();
+                    }
+                    if (choices.clearbit) {
+                      loadClearbit();
+                    }
+                  });
+                });
             `}
-            </Script>
-            <Script id="hotjar_analytics">
-              {`(function(h,o,t,j,a,r){
-                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-                h._hjSettings={hjid:4945188,hjsv:6};
-                a=o.getElementsByTagName('head')[0];
-                r=o.createElement('script');r.async=1;
-                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-                a.appendChild(r);
-                })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-            `}
-            </Script>
-          </>
+          </Script>
         )}
       </head>
       <body>
