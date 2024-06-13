689bcdb99e9593681d584d4a61db15c28473d73b
Olga <olga.volotovich@mail.ru>
2024-06-14 00:09:08 +0200

    refactor: testimonials order changes

--- src/app/(website)/enterprise/page.tsx
+++ src/app/(website)/enterprise/page.tsx
@@ -30,16 +30,16 @@ const logos = [
 
 const sliderItems = [
   {
-    text: 'Integrating Taipy into our suite of products for the Quick Service Restaurant (QSR) market has marked a significant step forward. The ease with which our development team was able to pick up Taipy was remarkable.',
-    authorName: 'Jesselynn Mah',
-    authorTitle: 'KnowledgeTouch',
-    logo: knowledgeTouchIcon,
-  },
-  {
     text: 'Diving into the CFM project with Taipy was a game-changer. We got our front-end and back-end up and running incredibly fast. It’s kind of amazing. And when you pair Taipy with Databricks, it’s like they’re two pieces of a puzzle that just fit perfectly together. We’re already looking at how we can use them for our next big AI and data projects.',
     authorName: 'Stephane Leray',
     authorTitle: 'Group les Mousquetaires',
     logo: lesMousquetairesIcon,
+  },
+  {
+    text: 'Integrating Taipy into our suite of products for the Quick Service Restaurant (QSR) market has marked a significant step forward. The ease with which our development team was able to pick up Taipy was remarkable.',
+    authorName: 'Jesselynn Mah',
+    authorTitle: 'KnowledgeTouch',
+    logo: knowledgeTouchIcon,
   },
 ];
 
