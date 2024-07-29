e3e67034f0d5300935554b2c5bc5be4883912605
iatopilskii <iatopilskii@gmail.com>
2024-07-29 16:12:11 +0200

    fix: fix testimonials content

--- src/constants/testimonials.ts
+++ src/constants/testimonials.ts
@@ -3,15 +3,15 @@ import lesMousquetairesIcon from '@/svgs/pages/pricing/les-mousquetaires.svg';
 
 export const TESTIMONIALS_ITEMS = [
   {
+    text: 'Implementing the CFM project with Taipy was a game-changer. We got our front-end and back-end up and running incredibly fast. It’s kind of amazing. And when you pair Taipy with Databricks, it’s like they’re two pieces of a puzzle that just fit perfectly together. We are already using Taipy as our standard platform for our new AI projects.',
+    authorName: 'Stephane Leray',
+    authorTitle: 'Group les Mousquetaires',
+    logo: lesMousquetairesIcon,
+  },
+  {
     text: 'Integrating Taipy into our suite of products for the Quick Service Restaurant (QSR) market has marked a significant step forward. The ease with which our development team was able to pick up Taipy was remarkable.',
     authorName: 'Jesselynn Mah',
     authorTitle: 'KnowledgeTouch',
     logo: knowledgeTouchIcon,
-  },
-  {
-    text: 'We are already using Taipy as our standard platform for our new AI projects.',
-    authorName: 'Stephane Leray',
-    authorTitle: 'Group les Mousquetaires',
-    logo: lesMousquetairesIcon,
   },
 ];
