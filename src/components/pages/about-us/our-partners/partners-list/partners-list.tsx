08925b379c07e6b0dc23b0165064eb75ebde2bea
obvankenoby <golova88@mail.ru>
2024-09-06 10:16:09 +0300

    complete update partners list

--- src/components/pages/about-us/our-partners/partners-list/partners-list.tsx
+++ src/components/pages/about-us/our-partners/partners-list/partners-list.tsx
@@ -2,22 +2,23 @@ import { SVGProps } from 'react';
 
 import clsx from 'clsx';
 
-import Bcgx from '@/svgs/logos/bcgx.inline.svg';
-import Corero from '@/svgs/logos/corero.inline.svg';
-import Datacamp from '@/svgs/logos/datacamp.inline.svg';
+import IBMWatson from '@/svgs/logos/partners/ibm-watson.inline.svg';
+import Databricks from '@/svgs/logos/partners/databricks.inline.svg';
 import Icb from '@/svgs/logos/icb.inline.svg';
 import Ifc from '@/svgs/logos/ifc.inline.svg';
 import Kdnuggets from '@/svgs/logos/kdnuggets.inline.svg';
-import Knowledge from '@/svgs/logos/knowledge.inline.svg';
-import Mousquetaires from '@/svgs/logos/les-mousquetaires.inline.svg';
-import Lsdirect from '@/svgs/logos/lsdirect.inline.svg';
-import Microsoft from '@/svgs/logos/microsoft.inline.svg';
-import Nylas from '@/svgs/logos/nylas.inline.svg';
+import Knowledge from '@/svgs/logos/partners/knowledge.inline.svg';
+import Mousquetaires from '@/svgs/logos/partners/les-mousquetaires.inline.svg';
+import Dataiku from '@/svgs/logos/partners/dataiku.inline.svg';
+import Microsoft from '@/svgs/logos/partners/microsoft.inline.svg';
 import Princeton from '@/svgs/logos/princeton-consultants.inline.svg';
-import Probtp from '@/svgs/logos/pro-btp.inline.svg';
 import Stevens from '@/svgs/logos/stevens.inline.svg';
-import Tal from '@/svgs/logos/tal-group.inline.svg';
+import Tal from '@/svgs/logos/partners/tal-group.inline.svg';
 import Ttc from '@/svgs/logos/ttc.inline.svg';
+import Awc from '@/svgs/logos/partners/aws.inline.svg';
+import Colab from '@/svgs/logos/partners/colab.inline.svg';
+import Suaee from "@/svgs/logos/partners/suaee.inline.svg";
+import Ifp from '@/svgs/logos/partners/ifp.inline.svg';
 
 const styles = {
   border: {
@@ -43,26 +44,26 @@ const partnersListData: Partner[] = [
     logo: Microsoft,
     alt: 'Microsoft',
     secondClass: 'border-r border-b xl:border-r-0 xl:border-b-0 2xs:border-b 2xs:border-r',
-    secondLogoClass: 'w-[183px] md:w-[144px] sm:w-[91px]',
+    secondLogoClass: 'w-[38px] md:w-[38px] sm:w-[38px]',
   },
   {
     logo: Mousquetaires,
-    alt: 'Mousquetaires',
+    alt: 'Les Mousquetaires',
     secondClass: 'border-r border-b xl:border-r-0 xl:border-b-0 2xs:border-b',
     secondLogoClass: 'w-[76px] md:w-[60px] sm:w-[38px]',
   },
   {
-    logo: Corero,
-    alt: 'Corero',
+    logo: Databricks,
+    alt: 'Databricks',
     secondClass:
       'border-r border-b lg:border-r-0 xl:border-r-0 xl:border-b-0 2xs:border-b 2xs:border-r',
-    secondLogoClass: 'w-[161px] md:w-[127px] sm:w-[80px]',
+    secondLogoClass: 'w-[47px] md:w-[47px] sm:w-[47px]',
   },
   {
-    logo: Lsdirect,
-    alt: 'Lsdirect',
+    logo: Dataiku,
+    alt: 'Dataiku',
     secondClass: 'border-b xl:border-b-0 2xs:border-b',
-    secondLogoClass: 'w-[203px] md:w-[160px] sm:w-[101px]',
+    secondLogoClass: 'w-[46px] md:w-[46px] sm:w-[46px]',
   },
   {
     logo: Princeton,
@@ -71,22 +72,22 @@ const partnersListData: Partner[] = [
     secondLogoClass: 'w-[171px] md:w-[135px] sm:w-[85px]',
   },
   {
-    logo: Bcgx,
-    alt: 'Bcgx',
+    logo: IBMWatson,
+    alt: 'IBM Watson',
     secondClass: 'border-r border-b xl:border-r-0 xl:border-b-0 2xs:border-b',
-    secondLogoClass: 'w-[136px] md:w-[107px] sm:w-[68px]',
+    secondLogoClass: 'w-[58px] md:w-[58px] sm:w-[58px]',
   },
   {
     logo: Ttc,
-    alt: 'Ttc',
+    alt: 'TTC AgriS',
     secondClass: 'border-r border-b xl:border-r-0 xl:border-b-0 2xs:border-b 2xs:border-r',
     secondLogoClass: 'w-[82px] md:w-[65px] sm:w-[41px]',
   },
   {
     logo: Knowledge,
-    alt: 'Predictive Layer',
+    alt: 'Knowledge Touch',
     secondClass: 'border-b xl:border-b-0 2xs:border-b',
-    secondLogoClass: 'w-[196px] md:w-[158px] sm:w-[106px]',
+    secondLogoClass: 'w-[44px] md:w-[44px] sm:w-[44px]',
   },
   {
     logo: Kdnuggets,
@@ -99,43 +100,43 @@ const partnersListData: Partner[] = [
     logo: Tal,
     alt: 'Tal Group',
     secondClass: 'border-r border-b xl:border-r-0 xl:border-b-0 2xs:border-b',
-    secondLogoClass: 'w-[110px] md:w-[79px] sm:w-[50px]',
+    secondLogoClass: 'w-[37px] md:w-[37px] sm:w-[37px]',
   },
   {
-    logo: Datacamp,
-    alt: 'Datacamp',
+    logo: Awc,
+    alt: 'AWS SageMaker',
     secondClass: 'border-r border-b xl:border-r-0 xl:border-b-0 2xs:border-b 2xs:border-r',
-    secondLogoClass: 'w-[168px] md:w-[132px] sm:w-[84px]',
+    secondLogoClass: 'w-[48px] md:w-[48px] sm:w-[48px]',
   },
   {
-    logo: Probtp,
-    alt: 'Probtp',
+    logo: Colab,
+    alt: 'Google Colab',
     secondClass: 'border-b xl:border-b-0 2xs:border-b 2xs:border-b',
-    secondLogoClass: 'w-[164px] md:w-[129px] sm:w-[82px]',
+    secondLogoClass: 'w-[76px] md:w-[76px] sm:w-[76px]',
   },
   {
     logo: Icb,
-    alt: 'Icb',
+    alt: 'ICB',
     secondClass: 'border-r xl:border-r-0 2xs:border-b 2xs:border-r',
     secondLogoClass: 'w-[50px] md:w-[43px] sm:w-[27px]',
   },
   {
-    logo: Nylas,
-    alt: 'Nylas',
+    logo: Suaee,
+    alt: 'SUAEE Group',
     secondClass: 'border-r xl:border-r-0 2xs:border-b',
-    secondLogoClass: 'w-[143px] md:w-[113px] sm:w-[71px]',
+    secondLogoClass: 'w-[42px] md:w-[42px] sm:w-[42px]',
   },
   {
     logo: Ifc,
-    alt: 'Ifc',
+    alt: 'IFC',
     secondClass: 'border-r xl:border-r-0',
     secondLogoClass: 'w-[157px] md:w-[124px] sm:w-[78px]',
   },
   {
-    logo: Stevens,
-    alt: 'Stevens',
+    logo: Ifp,
+    alt: 'IFP Energies nouvelles',
     secondClass: 'border-l xl:border-l-0 2xs:border-l',
-    secondLogoClass: 'w-[45px] md:w-[35px] sm:w-[22px]',
+    secondLogoClass: 'w-[109px] md:w-[109px] sm:w-[109px]',
   },
 ];
 
@@ -147,13 +148,14 @@ export default function PartnersList() {
       {partnersListData.map(({ logo: Logo, alt, secondClass, secondLogoClass }, index) => (
         <li
           className={clsx(
-            'relative flex h-[160px] w-full max-w-[319px] items-center justify-center border-grey-15 md:h-[126px] md:max-w-[256px] sm:h-[80px] sm:max-w-[159px]',
+            'relative flex flex-col gap-[30px] h-[160px] w-full max-w-[319px] items-center justify-center border-grey-15 md:h-[126px] md:max-w-[256px] sm:h-[80px] sm:max-w-[159px]',
             secondClass,
             buttonClassName,
           )}
           key={index}
         >
           <Logo className={clsx('fill-grey-70', secondLogoClass)} alt={alt} />
+          <div className="text-[#797986] text-[14px]">{alt}</div>
         </li>
       ))}
     </ul>
