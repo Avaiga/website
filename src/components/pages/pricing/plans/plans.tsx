319a3bd6fe4b29c8e1d2341abcc50daeb792a66c
Nevo David <nevo@notu.co>
2024-10-07 15:35:19 +0700

    feat: margin top

--- src/components/pages/pricing/plans/plans.tsx
+++ src/components/pages/pricing/plans/plans.tsx
@@ -6,7 +6,7 @@ import PricingVariant from './pricing-variant';
 
 function Plans({ heading, card1, card2, card3 }: PlansProps) {
   return (
-    <section className="plans mt-6 lg:mt-14 md:mt-12 sm:mt-10">
+    <section className="plans mt-[184px] px-safe lg:mt-[184px] md:mt-[154px] md:pb-[72px] sm:mt-[129px] sm:pb-[66px]">
       <div className="container flex max-w-7xl flex-col items-center gap-y-11 lg:gap-y-9 md:gap-y-8 sm:gap-y-7">
         <Heading text={heading} />
         <div className="grid grid-cols-3 gap-x-8 lg:gap-x-6 md:grid-cols-1 md:gap-y-6 sm:gap-y-5">
