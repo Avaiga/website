4d61786263b2fec8bd1c28cbc496c5d9f14b2058
Nevo David <nevo@notu.co>
2024-10-07 15:25:06 +0700

    feat: comment book a call

--- src/components/pages/pricing/hero/hero.tsx
+++ src/components/pages/pricing/hero/hero.tsx
@@ -15,15 +15,15 @@ export default function Hero({ tag, heading, description, buttonText, buttonLink
         <p className="mx-auto max-w-[642px] pt-5 leading-snug opacity-80 lg:max-w-xl md:pt-4 sm:pt-3.5 sm:text-14">
           {description}
         </p>
-        <Button
-          className="relative z-10 mt-[46px] w-full max-w-[180px] lg:mt-7 sm:mt-[22px]"
-          size="lgConst"
-          theme="red-filled"
-          href={buttonLink}
-        >
-          {buttonText}
-          <span className="absolute top-[-51px] -z-10 h-[190px] w-full max-w-9 rotate-90 bg-hero-btn-enterprise blur-[36px]" />
-        </Button>
+        {/*<Button*/}
+        {/*  className="relative z-10 mt-[46px] w-full max-w-[180px] lg:mt-7 sm:mt-[22px]"*/}
+        {/*  size="lgConst"*/}
+        {/*  theme="red-filled"*/}
+        {/*  href={buttonLink}*/}
+        {/*>*/}
+        {/*  {buttonText}*/}
+        {/*  <span className="absolute top-[-51px] -z-10 h-[190px] w-full max-w-9 rotate-90 bg-hero-btn-enterprise blur-[36px]" />*/}
+        {/*</Button>*/}
       </div>
     </section>
   );
